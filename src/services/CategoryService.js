6c8fcc75064e8f23a3bc5dfc996070a14441a8eb
tonhue25 <tonhue25@gmail.com>
2022-08-26 00:48:43 +0700

    fix : update category service

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -1,4 +1,5 @@
 import { request_admin, request_public } from './base_url';
+
 export const getAllCategories = async () => {
     const res = await request_public.get('/categories');
     return res;
